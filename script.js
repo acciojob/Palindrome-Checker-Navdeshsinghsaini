2ad268bf35dc85a72f9f2149cd5de92b7f07951c
Navdeshsinghsaini <naveshsinghsaini@gmail.com>
2023-01-23 22:02:12 +0530

    solution

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+  var one = str.replace (/[^A-Z0-9]/ig , "").toLowerCase();
 
+   var two = one.split('').reverse.join('');
+
+   if(one == two){
+    return true;
+   }else {
+    return false;
+   }
 }
 module.exports = palindrome
